9465550ada4ac446130ba001f9157c2b99c7c14e
Sean <spcasey99@gmail.com>
2020-07-08 23:45:18 +0000

    Add map function and update page layout

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -83,3 +83,66 @@ function getTranslation() {
     resultContainer.innerText = translatedMessage;
   });
 }
+
+/*
+ * Creates a map of South Bend (night mode) and adds it to the page.
+ */ 
+function generateMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.6764, lng: -86.2520}, zoom: 6,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {featureType: 'administrative.locality', elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]},
+      {featureType: 'poi', elementType: 'labels.text.fill', 
+        stylers: [{color: '#d59563'}]},
+      {featureType: 'poi.park', elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]},
+      {featureType: 'poi.park', elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]},
+      {featureType: 'road', elementType: 'geometry',
+        stylers: [{color: '#38414e'}]},
+      {featureType: 'road', elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]},
+      {featureType: 'road', elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]},
+      {featureType: 'road.highway', elementType: 'geometry',
+        stylers: [{color: '#746855'}]},
+      {featureType: 'road.highway', elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]},
+      {featureType: 'road.highway', elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]},
+      {featureType: 'transit', elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]},
+      {featureType: 'transit.station', elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]},
+      {featureType: 'water', elementType: 'geometry',
+        stylers: [{color: '#17263c'}]},
+      {featureType: 'water', elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]},
+      {featureType: 'water', elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]}
+    ]
+    
+  });
+  
+  const houseMarker = new google.maps.Marker({
+    position: {lat: 41.750880, lng: -86.146510},
+    map: map,
+    title: 'My House'
+  });
+
+  const HighSchoolMarker = new google.maps.Marker({
+    position: {lat: 41.681751, lng: -86.238419},
+    map: map,
+    title: 'My High School'
+  });
+
+  const PFAHQMarker = new google.maps.Marker({
+    position: {lat: 41.674830, lng: -86.249850},
+    map: map,
+    title: 'PFA Headquarters'
+  });
+}
